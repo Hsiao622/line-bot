db2f0978188f2cc4beec260749f232ecf4d293aa
Chris Hsiao <march0622@yahoo.com.tw>
2022-03-20 01:58:04 +0800

    update

--- app.py
+++ app.py
@@ -39,7 +39,7 @@ def handle_message(event):
     msg = event.message.text
     r = '很抱歉，您說什麼'
 
-    if msg == ['hi', 'Hi']:
+    if msg in ['hi', 'Hi']:
         r = '安安'
     elif msg == '你吃飯了嗎':
         r = '還沒'
